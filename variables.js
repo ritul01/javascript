fadfbec487807567071cf830631f3f35efe212be
Singh <ritulsingh123700@gmail.com>
2024-07-01 23:25:53 +0530

    changes

--- variables.js
+++ variables.js
@@ -5,6 +5,10 @@ accountCity="Partawal"
 let accountState;
 
 // accountId=2    not allowed
+/*
+prefer not to use var bcoz of issue in block scopeand functional scope
+*/
+
 accountEmail="rs@gmail.com"
 accountPassword="11111"
 accountCity="gkp"
