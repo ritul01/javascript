4e5693f2c97f42e4f5a4efa5467acfa6f57c5f3a
Singh <ritulsingh123700@gmail.com>
2024-07-01 23:20:44 +0530

    test

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accountId=1234
+let accountEmail="Singh@gmail.com"
+var accountPassword="00000"
+accountCity="Partawal"
+let accountState;
+
+// accountId=2    not allowed
+accountEmail="rs@gmail.com"
+accountPassword="11111"
+accountCity="gkp"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
