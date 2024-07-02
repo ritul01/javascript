c00d3c825ab3bca08763b44b160d39db6d4c5b07
Singh <ritulsingh123700@gmail.com>
2024-07-02 22:28:35 +0530

    some changes

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,11 +1,11 @@
 let score="singh"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 /*
 "35"=>35
@@ -15,7 +15,7 @@ true=>1; false=>0
 
 let isLoggedIn="singh"
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 /*
 1=>true; 0=>false
@@ -25,5 +25,29 @@ console.log(booleanIsLoggedIn)
 
 let num=31
 let stringNum=String(num)
-console.log(stringNum)
-console.log(typeof stringNum)
+// console.log(stringNum)
+// console.log(typeof stringNum)
+
+// <------------operation------------->
+let value=3;
+let negValue=-value
+// console.log(negValue)
+
+let str1="hello"
+let str2=" singh"
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(+true)
+// console.log(+"")
+
+let num1,num2,num3;
+num1=num2=num3=2+2
+let gameCounter=100
+++gameCounter
+console.log(gameCounter)
