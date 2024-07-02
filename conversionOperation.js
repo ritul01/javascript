766752242fd299a16d78a6dcd78e37f39b45cc62
Singh <ritulsingh123700@gmail.com>
2024-07-02 12:46:11 +0530

    conversion and operation

---
+++ conversionOperation.js
@@ -0,0 +1,29 @@
+let score="singh"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+/*
+"35"=>35
+"35abs"=>NaN
+true=>1; false=>0
+*/
+
+let isLoggedIn="singh"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+/*
+1=>true; 0=>false
+""=>false
+"singh"=>true
+*/
+
+let num=31
+let stringNum=String(num)
+console.log(stringNum)
+console.log(typeof stringNum)
