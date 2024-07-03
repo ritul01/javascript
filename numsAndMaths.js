d5abb5b6fd1bfcf490e9b9230d54a2b5f5d85da3
Singh <ritulsingh123700@gmail.com>
2024-07-03 18:52:12 +0530

    nums ans maths

---
+++ numsAndMaths.js
@@ -0,0 +1,30 @@
+const score=1000
+// console.log(score)
+
+const balance=new Number(200)
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(3))
+
+const num2=123.72342
+// console.log(num2.toPrecision(4))
+
+const num3 =1000000
+// console.log(num3.toLocaleString('en-IN'))
+
+// ----------------MAths---------------------
+// console.log(Math.round(1.3))
+// console.log(Math.abs(-4))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(1,6,4,3,8))
+// console.log(Math.max(1,6,4,3,8))
+console.log(Math.random())
+console.log(Math.random()*10+1)
+console.log(Math.floor(Math.random()*10)+1)
+// console.log(Math)
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
