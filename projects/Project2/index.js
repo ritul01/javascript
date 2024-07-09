876bab9442bc315f16a4d2e166e2de34cf762c62
Singh <ritulsingh123700@gmail.com>
2024-07-09 22:32:18 +0530

    project 2

---
+++ projects/Project2/index.js
@@ -0,0 +1,27 @@
+const form=document.querySelector('form')
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const result = document.querySelector('#result');
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        result.innerHTML = `Please give a valid height ${height}`;
+      } else if (weight === '' || weight < 0 || isNaN(weight)) {
+        result.innerHTML = `Please give a valid weight ${weight}`;
+      } else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        //show the result
+        if(bmi<18.5){
+            result.innerHTML = `<span>${bmi}</span> : You are Under Weight`;
+        }else if(bmi>=18.5 && bmi<=24.9){
+            result.innerHTML = `<span>${bmi}</span> : You are Normal`;
+        }else{
+            result.innerHTML = `<span>${bmi}</span> : You are Over Weight`;
+        }
+      }
+})
